ed4f33eb4dae1df5ba2e996ec6fbd8056e9e326b
pradeeppvj8 <pradeep.pvj8@gmail,com>
2023-08-30 21:40:05 +0530

    setup and requirements added

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [ req.replace("\n","") for req in requirements]
+
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+    
+setup(
+name='Students-Performance-Prediction',
+version='0.0.1',
+author='Pradeep.P',
+author_email="pradeep.pvj8@gmail.com",
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
