97b8ca4a6613ada9ac100a41c64ca14b67b8ee8e
pradeeppvj8 <pradeep.pvj8@gmail,com>
2023-09-03 19:24:35 +0530

    Hyperparameter tuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,8 +46,45 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
 
+            params = {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "K-Neighbors Regressor":{}
+            }
+
             model_report : dict = evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test,
-                                                  y_test = y_test, models = models)
+                                                  y_test = y_test, models = models, params = params)
             
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
@@ -56,7 +93,7 @@ class ModelTrainer:
             if best_model_score < 0.6:
                 raise CustomException("No best model found")
             
-            logging.info("Best model found")
+            logging.info("Best model found " + best_model_name)
 
             save_object(
                 file_path = self.model_trainer_config.trained_model_file_path,
--- src/utils.py
+++ src/utils.py
@@ -4,28 +4,49 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from src.logger import logging
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
 
         os.makedirs(dir_path, exist_ok=True)
-        
+
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+            model_name = list(models.keys())[i]
+
+            logging.info("Performing hyper paramter tuning for : " + model_name)
+
+            # Perform hyper parameter tuning using grid search cv
+            gs = GridSearchCV(model, param, cv = 5)
+            gs.fit(X_train, y_train)
+
+            logging.info("Hyper paramter tuning finished for : " + model_name)
+
+            # Fit the model based on the best params found by grid search cv
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
+            # Perform predictions on test set
             y_test_pred = model.predict(X_test)
+
+            # Find the r2 score on test set
             test_model_score = r2_score(y_test, y_test_pred)
             report[list(models.keys())[i]] = test_model_score
+
+            logging.info("Model training and evaluation done for : " + model_name)
         return report
     except Exception as e:
         raise CustomException(e, sys)
