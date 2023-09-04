700500f6c09c7e2f9ce24fdd8ad7f4f0790b5366
pradeeppvj8 <pradeep.pvj8@gmail,com>
2023-09-04 23:12:14 +0530

    Added python.config file for aws elasticbeanstalk deployment

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def predict():
         return render_template('home.html', results = results[0])
     
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", debug=True)
+    app.run(host = "0.0.0.0")
